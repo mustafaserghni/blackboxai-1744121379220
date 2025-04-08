a4ed74fd73654ad9b532c3fe8a3bc92c7230bc73
Mostafa Serghini <mustdev@menara.ma>
2025-04-08 22:26:43 +0100

    Add al-mizan-lms/backend/src/modules/case-management/dto/update-case.dto.ts 1744147604178

--- al-mizan-lms/backend/src/modules/case-management/dto/update-case.dto.ts
+++ al-mizan-lms/backend/src/modules/case-management/dto/update-case.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateCaseDto } from './create-case.dto';
 
-export class UpdateCaseDto extends PartialType(CreateCaseDto) {}
+// We extend PartialType to make all fields optional for updates
+// We omit caseNumber since it shouldn't be updated after creation
+export class UpdateCaseDto extends PartialType(
+  OmitType(CreateCaseDto, ['caseNumber'] as const),
+) {}
