fde07d9428aa879ae37fff83d4de3c324e1b5b20
Mostafa Serghini <mustdev@menara.ma>
2025-04-08 22:26:49 +0100

    Add al-mizan-lms/backend/src/modules/case-management/case-management.module.ts 1744147609756

--- al-mizan-lms/backend/src/modules/case-management/case-management.module.ts
+++ al-mizan-lms/backend/src/modules/case-management/case-management.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CaseManagementController } from './case-management.controller';
 import { CaseManagementService } from './case-management.service';
+import { CaseTransformer } from './transformers/case.transformer';
 import { Case } from './entities/case.entity';
 import { Client } from './entities/client.entity';
 import { Party } from './entities/party.entity';
 import { CaseEvent } from './entities/case-event.entity';
 import { CaseDeadline } from './entities/case-deadline.entity';
 import { CaseTeamMember } from './entities/case-team-member.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [
@@ -18,10 +20,11 @@ import { CaseTeamMember } from './entities/case-team-member.entity';
       CaseEvent,
       CaseDeadline,
       CaseTeamMember,
+      User,
     ]),
   ],
   controllers: [CaseManagementController],
-  providers: [CaseManagementService],
+  providers: [CaseManagementService, CaseTransformer],
   exports: [CaseManagementService],
 })
 export class CaseManagementModule {}
