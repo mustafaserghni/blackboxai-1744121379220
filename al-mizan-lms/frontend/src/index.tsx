8b5dc1ae32df072cea5d89fca38ae539c17e660d
Mostafa Serghini <mustdev@menara.ma>
2025-04-08 17:00:54 +0100

    Add al-mizan-lms/frontend/src/index.tsx 1744128055561

---
+++ al-mizan-lms/frontend/src/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { store } from './store';
+import { theme } from './theme';
+import './i18n';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
